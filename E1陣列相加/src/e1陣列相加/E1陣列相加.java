9fc67f74bc1c5d8c8eba9a71e778ce3f5701aa1a
z7879516 <51859955+z7879516@users.noreply.github.com>
2019-06-16 00:29:27 +0800

    Add files via upload

---
+++ E1陣列相加/src/e1陣列相加/E1陣列相加.java
@@ -0,0 +1,43 @@
+﻿
+package e1陣列相加;
+import java.util.Scanner;
+public class E1陣列相加
+{
+    public static void main(String[] args) 
+    {
+    Scanner a=new Scanner(System.in);
+    int b[][]=new int[3][3];
+    int c[][]=new int[3][3];
+    int d;
+    System.out.println("請輸入第1個陣列:");
+    for(int x=0;x<3;x++)
+    {
+        for(int y=0;y<3;y++)
+      {  
+        d=a.nextInt();
+        b[x][y]=d;
+      }
+    }
+    System.out.println("請輸入第2個陣列:");
+     for(int x=0;x<3;x++)
+    {
+        for(int y=0;y<3;y++)
+      {
+        d=a.nextInt();
+        c[x][y]=d;
+      }
+    }
+    System.out.println("陣列加總值:");
+    int g[][]=new int[3][3];
+      for(int x=0;x<3;x++)
+    {
+        for(int y=0;y<3;y++)
+      {
+          g[x][y]=b[x][y]+c[x][y];
+          System.out.print(g[x][y]+" ");
+      }
+         System.out.print("\n");
+    }
+      
+    }
+}
